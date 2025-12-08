adf0cea0037535cb0e7f1c8d739174308b2a3ede
Felix Broberg <felix.broberg29@gmail.com>
2025-12-08 16:30:19 +0100

    list stocks

--- portfolio-front/src/components/sidebar.jsx
+++ portfolio-front/src/components/sidebar.jsx
@@ -1,11 +1,14 @@
-const sidebar = () => {
-    const stocks = ["AAPL", "GOOG"]
+export const Sidebar = () => {
+    const stocks = [
+        { symbol: "AAPL", price: 189.25 },
+        { symbol: "GOOG", price: 135.42 }
+    ];
 
     return (
         <>
             <ul>
                 {stocks.map(s => (
-                    <li>{s}</li>
+                    <li key={s.symbol}>{s.symbol} price: {s.price}</li>
                 ))}
             </ul>
             <button>+</button>
